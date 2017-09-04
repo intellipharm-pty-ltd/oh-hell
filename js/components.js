9654a0a6fd05aab5ed3ffacdd4039e5696b177d7
tim-intellipharm <timothy.ruhle@intellipharm.com.au>
2017-09-04 11:10:22 +1000

    Fixing up all icons and leaderboard layout

--- js/components.js
+++ js/components.js
@@ -24,7 +24,7 @@ var suitComponent = {
 			<!-- &#127183; -->
 			<img src="http://www.peacemonger.org/assets/images/CS155-X.jpg" height="185px" alt="No Trumps" />
 		</span>
-		<span ng-if="!suit.isNoTrumps()" ng-class="'suit-' + suit.suit + ' suits-' + suit.suit"></span>`,
+		<span ng-if="!suit.isNoTrumps()" ng-class="'suit-' + suit.suit + ' icons-suit-' + suit.suit" style="font-size:0.75em;"></span>`,
 	controller: function() {
 		this.isNoTrumps = function() {
 			return this.suit === 'N';
